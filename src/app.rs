5929a32c70f09700d60b0a6b85cacac9fb274f64
Lucas A <game4allyt@gmail.com>
2020-12-06 11:53:15 +0100

    Add AppState

---
+++ src/app.rs
@@ -0,0 +1,32 @@
+use pixels::{Pixels, SurfaceTexture};
+use std::error;
+use winit::{
+    event::WindowEvent,
+    window::Window,
+};
+
+pub struct AppState {
+    pixels: Pixels<Window>,
+}
+
+impl AppState {
+    pub fn create(window: &Window) -> Result<AppState, Box<dyn error::Error>> {
+        let win_size = window.inner_size();
+        let surface = SurfaceTexture::new(win_size.width, win_size.height, window);
+        let pixels = Pixels::new(win_size.width, win_size.height, surface)?;
+
+        Ok(AppState { pixels: pixels })
+    }
+
+    pub fn draw(&mut self) {
+        self.pixels.render().unwrap();
+    }
+
+    pub fn handle_input(&mut self, event: WindowEvent) {
+        println!("Handled event: {:?}", event);
+    }
+
+    pub fn update(&mut self) {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("hello");
+mod app;
+
+use std::error;
+
+use winit::{event::Event, event_loop::EventLoop, window::WindowBuilder};
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut app = app::AppState::create(&window)?;
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::MainEventsCleared => {
+            app.update();
+            window.request_redraw();
+        }
+        Event::RedrawRequested(_) => {
+            app.draw();
+        }
+        Event::WindowEvent { event, .. } => {
+            app.handle_input(event);
+        }
+        _ => (),
+    });
 }
